3bf79b38a5e2433407f0c968fe33863d5e05ff20
diegojimenez77 <djpcoding8652@gmail.com>
2023-05-25 14:18:05 -0600

    adding user register connected to mongo db and bcrypt password

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,24 @@
 const router = require("express").Router();
-
-router.get("/",(req, res) => {
-    res.send("hey its auth route");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+//REGISTER
+router.post("/register", async (req, res) => {
+    try{
+        //generate new password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        //create mew user
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+        //save user and respond
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch(err){
+        console.log(err)
+    }
 });
 
 module.exports =router;
